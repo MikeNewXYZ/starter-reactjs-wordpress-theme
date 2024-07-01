995f817f5ba4c12670976f7c755202f0a03858a9
Mike Newberry <hello@mikenew.xyz>
2024-07-01 19:08:10 +0100

    setup page data fetch

--- src/routes/PageRoute/PageRoute.jsx
+++ src/routes/PageRoute/PageRoute.jsx
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "@wordpress/element";
 import Content from "../../components/Content/Content";
+import getPageData from "../../api/getPageData/getPageData";
 
 export default function PageRoute() {
 	const [data, setData] = useState(null);
+	const [error, setError] = useState({ isError: false, errorMessage: "" });
 
-	useEffect(() => getData(), []);
+	// Fetch page data
+	useEffect(() => {
+		const getData = async () => {
+			const response = await getPageData(wpData.theId);
 
-	const getData = async () => {
-		const reponse = await fetch(`${wpData.baseUrl}/wp-json/wp/v2/pages/${wpData.theId}`);
-		const data = await reponse.json();
+			if (!response.success) {
+				setError({ isError: true, errorMessage: response.error });
+				return;
+			}
 
-		setData(data);
-	};
+			setData(response.data);
+		};
+		getData();
+	}, []);
 
+	// Render error message if fetching page data fails
+	if (error.isError) {
+		return <p>ERROR: {error.errorMessage}</p>;
+	}
+
+	// Render loading message when page is still loading data
 	if (!data) {
 		return <p>loading</p>;
 	}
 
+	// Render main content if page data is successfully fetched
 	return (
 		<main className="container mx-auto">
-			<h1 className="text-3xl font-bold underline">{data.title.rendered}</h1>
-			<Content data={data.content.rendered} />
+			<Content data={`<h1>${data.title.rendered}</h1>${data.content.rendered}`} />
 		</main>
 	);
 }
