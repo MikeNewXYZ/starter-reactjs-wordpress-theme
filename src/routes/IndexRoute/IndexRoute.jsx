717cfb9bbd1e03b59d3105d6817d4a5a856aa88f
Mike Newberry <hello@mikenew.xyz>
2024-07-06 17:28:55 +0100

    add index route component

--- src/routes/IndexRoute/IndexRoute.jsx
+++ src/routes/IndexRoute/IndexRoute.jsx
@@ -1,3 +1,12 @@
+import ContentContainer from "../../components/ContentContainer/ContentContainer";
+
 export default function IndexRoute() {
-	return <h1>home page</h1>;
+	const frontData = wpData.pageData.front;
+
+	return (
+		<>
+			<h1>This is a Index/Home Route</h1>
+			<ContentContainer content={frontData.post_content} />
+		</>
+	);
 }
