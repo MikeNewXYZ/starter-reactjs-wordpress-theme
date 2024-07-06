8cb174177c7dcaaed0fae7c784d975196623946d
Mike Newberry <hello@mikenew.xyz>
2024-07-06 17:26:16 +0100

    add page and post route component

--- src/routes/PageRoute/PageRoute.jsx
+++ src/routes/PageRoute/PageRoute.jsx
@@ -1,3 +1,12 @@
+import ContentContainer from "../../components/ContentContainer/ContentContainer";
+
 export default function PageRoute() {
-	return <h1>This is a Page Route</h1>;
+	const pageData = wpData.pageData.current;
+
+	return (
+		<>
+			<h1>This is a Page Route</h1>
+			<ContentContainer content={pageData.post_content} />
+		</>
+	);
 }
--- src/routes/PostRoute/PostRoute.jsx
+++ src/routes/PostRoute/PostRoute.jsx
@@ -1,3 +1,12 @@
+import ContentContainer from "../../components/ContentContainer/ContentContainer";
+
 export default function PostRoute() {
-	return <h1>This is a Post Route</h1>;
+	const postData = wpData.pageData.current;
+
+	return (
+		<>
+			<h1>This is a Post Route</h1>
+			<ContentContainer content={postData.post_content} />
+		</>
+	);
 }
